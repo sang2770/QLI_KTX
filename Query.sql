e75ade0d6a752ce154efe732baab79228453e116
hunmui <hung71718@gmail.com>
2021-10-15 16:58:32 +0700

    update file query.sql

--- Query.sql
+++ Query.sql
@@ -1,1 +1,19 @@
 use QLI_KTX
+
+create function count_sl(@maphong nvarchar(20))
+returns int
+as
+begin
+	declare @sl int
+	select @sl=count(MaHopDong)
+	from TB_HOPDONG
+	where @maphong=Ma_Phong
+return @sl
+end
+
+update TB_Phong
+set SL_DANG_O=  dbo.count_sl(N'P001')
+
+update TB_Phong
+set SL_NGUOI_MAX=8
+
